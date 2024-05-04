56834dd16bf34f03e640498d70a69fb288442466
Astrid Boronda Heidinger <108700549+AstridBH@users.noreply.github.com>
2024-05-03 20:59:48 -0500

    feat: add footer component

---
+++ components/footer-component.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <div class="footer">
+      <p>Copyright © 2024 Guardian IQ, inc. All rights reserved.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "footer"
+  }
+  </script>
+  
+  <style scoped>
+  </style>
