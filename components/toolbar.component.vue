ad5394129feb740a82b87bfd75b29bc3a3e9dc05
Astrid Boronda Heidinger <108700549+AstridBH@users.noreply.github.com>
2024-05-03 20:52:24 -0500

    fead: add toolbar component

---
+++ components/toolbar.component.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="toolbar">
+      <div class="tituloybarra">
+        <h1 class="toolbar-title">Guardian IQ</h1>
+        <div class="search-bar">
+          <input type="text" placeholder="Buscar...">
+          <button>Buscar</button>
+        </div>
+      </div>
+    </header>
+    <div class="toolbar2">
+      <div class="button-container">
+        <button>Home</button>
+        <button>Contratar personal</button>
+        <button>Servicios activados</button>
+        <button>Equipos de seguridad</button>
+      </div>
+    </div>
+  
+  </template>
