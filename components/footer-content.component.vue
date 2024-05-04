40cbf9e114b04ed56426451d1cac4868fe1f6fe7
rivacortezf <u202215313@upc.edu.pe>
2024-05-03 22:56:26 -0500

    feat(personalsafety): added footer & toolbar

---
+++ components/footer-content.component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer">
+    <p>Copyright © 2024 Guardian IQ, inc. All rights reserved.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer-content"
+}
+</script>
+
+<style scoped>
+.footer {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
---
+++ pages/personalSafety.vue
@@ -0,0 +1,84 @@
+
+
+<script>
+import Toolbar from "../components/Toolbar-component.vue";
+
+export default {
+  components: {Toolbar},
+  data() {
+    return {
+      guards: []
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetch('https://my-json-server.typicode.com/rivacortez/personsafe/personalsafety');
+        const data = await response.json();
+        this.guards = data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <toolbar></toolbar>
+  <div class="guard-grid">
+    <div class="guard-card" v-for="guard in guards" :key="guard.nombre">
+      <img class="guard-photo" :src="guard.foto" alt="Foto de {{ guard.nombre }}" />
+      <h2 class="guard-name">{{ guard.nombre }}</h2>
+      <p class="guard-district">{{ guard.distrito }}</p>
+      <p class="guard-age">Edad: {{ guard.edad }}</p>
+      <p class="guard-sex">Sexo: {{ guard.sexo }}</p>
+      <p class="guard-experience">Experiencia: {{ guard.experiencia }}</p>
+      <p class="guard-availability">Disponibilidad: {{ guard.disponibilidad }}</p>
+      <button class="guard-button">Contratar</button>
+      <button class="guard-button">Contactar</button>
+    </div>
+  </div>
+</template>
+<style >
+.guard-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 20px;
+  background-color: #11315C;
+}
+
+.guard-card {
+  background-color: #f8f8f8;
+  border-radius: 10px;
+  width: 300px;
+  margin: 10px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.guard-photo {
+  width: 200px;
+  height:200px;
+}
+
+.guard-button {
+  background-color: #F9A825;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 5px;
+}
+</style>
