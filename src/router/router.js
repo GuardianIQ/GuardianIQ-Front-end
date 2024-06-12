f68b3d43555dd99211277e60e13fd40b667aa937
rivacortezf <u202215313@upc.edu.pe>
2024-06-11 20:06:34 -0500

    fix: change router

--- src/router/router.js
+++ src/router/router.js
@@ -1,31 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import safetyDevicePageComponent from "@/securityContext/pages/SafetyDevice/SafetyDevice-page.component.vue";
-import loginContentComponent from "@/public/components/Login/login.component.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import SafetyDevicePageComponent from "../securityContext/pages/SafetyDevice/SafetyDevice-page.component.vue";
+import LoginComponent from "../public/components/Login/login.component.vue";
+import PersonalCardPageComponent from "../securityContext/pages/PersonalSafety/PersonalCard-page.component.vue";
+import ShoppingDeviceCard from "../securityContext/components/SafetyDeviceCard-component/shoppingdeviceCard.vue";
+import OrderListPageComponent from "../securityContext/pages/OrderList/OrderList-page.component.vue";
+import TechnicalSupportPageComponent from "../public/pages/TechnicalSupport-page.component.vue";
+import RegisterComponent from "../public/components/Register/register.component.vue";
+import FormSupport from "../public/pages/FormSupport.vue";
+import payment from "../public/components/payment/payment.vue";
+const routes = [
+  { path: '/', component: () => import('../public/pages/home-page.component.vue') },
+  { path: '/home', component: () => import('../public/pages/home-page.component.vue') },
+  { path: '/:pathMatch(.*)*', component: () => import('../public/pages/NotFound-page.component.vue') },
+  { path: '/PersonalCardPage', component: PersonalCardPageComponent },
+  { path: '/SafetyDevicePage', component: SafetyDevicePageComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/carrito', component: ShoppingDeviceCard },
+  { path: '/orderList', component: OrderListPageComponent },
+  { path: '/formSupport', component: TechnicalSupportPageComponent },
+  { path: '/register', component: RegisterComponent },
+  { path: '/formSupport', component: FormSupport },
+  {path: '/payment', component: payment}
+];
 
-import personalCardPageComponent from "@/securityContext/pages/PersonalSafety/PersonalCard-page.component.vue";
-import shoppingdeviceCard from "@/securityContext/components/SafetyDeviceCard-component/shoppingdeviceCard.vue";
-import tutorialManagementComponent from "@/buyContext/pages/tutorial-management.component.vue";
-import orderListPageComponent from "@/securityContext/pages/OrderList/OrderList-page.component.vue";
-import technicalSupportPageComponent from "@/public/pages/TechnicalSupport-page.component.vue";
-import registerComponent from "@/public/components/Register/register.component.vue";
-import formSupport from "@/public/pages/FormSupport.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {path:'', component:()=>import('../public/pages/home-page.component.vue')},
-    {path:'/home', component:()=>import('../public/pages/home-page.component.vue')},
-    {path:'/:pathMatch(.*)*', component:()=>import('../public/pages/NotFound-page.component.vue')},
-    {path:'/PersonalCardPage',component:personalCardPageComponent},
-    {path:'/SafetyDevicePage',component:safetyDevicePageComponent},
-    {path: '/login', component: loginContentComponent},
-    {path: '/carrito', component: shoppingdeviceCard},
-    {patch: '/tutorials',component: tutorialManagementComponent, meta: { title: 'Tutorials' } },
-    {path: '/orderList', component: orderListPageComponent},
-    {patch: '/formSupport', component: technicalSupportPageComponent},
-    {patch: '/register',component: registerComponent},
+  routes
+});
 
-    { path: '/formSupport', component: formSupport}
-  ]
-})
-
-export default router
+export default router;
